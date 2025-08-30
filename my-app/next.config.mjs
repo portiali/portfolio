eed6ea61527ca23f076863ed1a2d561a35b76090
portia li <your_new_email@example.com>
2025-08-30 02:26:18 -0400

    WIP: save current changes

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -6,13 +6,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/portfolio',   
-    assetPrefix: '/portfolio/', 
+    basePath: '/portfolio',
+    assetPrefix: '/portfolio/',
   };
   
-  module.exports = nextConfig;
+  export default nextConfig;
+  
   
   
